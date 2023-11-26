e159230beecff17b748f2e0ce4a3043e30fad5fc
southorange0929 <southorange0929@foxmail.com>
2023-11-26 10:01:40 +0800

    feat: add basic value example

--- example/basic/src/lib.rs
+++ example/basic/src/lib.rs
@@ -1,12 +1,50 @@
 use napi_derive_ohos::napi;
-use napi_ohos::bindgen_prelude::pre_init;
-use napi_ohos::module_init;
+use napi_ohos::bindgen_prelude::{pre_init, BigInt, Buffer, Null, Object, Undefined};
+use napi_ohos::{module_init, Env};
 
 #[napi]
-pub fn add(left: u32, right: u32) -> u32 {
-  left + right
+pub fn get_bool() -> bool {
+    false
 }
+
+#[napi]
+pub fn get_number() -> i64 {
+    100 as i64
+}
+
+#[napi]
+pub fn get_undefined() -> Undefined {
+    ()
+}
+
+#[napi]
+pub fn get_null() -> Null {
+    Null
+}
+
+#[napi]
+pub fn get_array() -> Vec<u32> {
+    vec![1, 2, 3]
+}
+
+#[napi]
+pub fn get_object(env: Env) -> Object {
+    let mut obj = env.create_object().unwrap();
+    obj.set("test", 1).unwrap();
+    obj
+}
+
+#[napi]
+pub fn get_array_buffer() -> Buffer {
+    vec![1, 2, 3].into()
+}
+
+#[napi]
+pub fn bigint_add(a: BigInt, b: BigInt) -> u128 {
+    a.get_u128().1 + b.get_u128().1
+}
+
 #[module_init]
 fn init() {
-  pre_init();
+    pre_init();
 }
