a7142cf1050437ad459768ac080f55e6026fc689
southorange0929 <southorange0929@foxmail.com>
2023-12-03 12:58:09 +0800

    feat: add global example

---
+++ example/global/src/lib.rs
@@ -0,0 +1,34 @@
+use napi_derive_ohos::{js_function, module_exports};
+use napi_ohos::bindgen_prelude::pre_init;
+use napi_ohos::module_init;
+
+use std::convert::TryInto;
+
+use napi_ohos::{CallContext, JsFunction, JsNumber, JsObject, JsTimeout, JsUndefined, Result};
+
+#[js_function(2)]
+pub fn set_timeout(ctx: CallContext) -> Result<JsTimeout> {
+    let handler: JsFunction = ctx.get(0)?;
+    let timeout: JsNumber = ctx.get(1)?;
+    ctx.env
+        .get_global()?
+        .set_timeout(handler, timeout.try_into()?)
+}
+
+#[js_function(1)]
+pub fn clear_timeout(ctx: CallContext) -> Result<JsUndefined> {
+    let timer: JsTimeout = ctx.get(0)?;
+    ctx.env.get_global()?.clear_timeout(timer)
+}
+
+#[module_exports]
+pub fn register_js(mut exports: JsObject) -> Result<()> {
+    exports.create_named_method("setTimeout", set_timeout)?;
+    exports.create_named_method("clearTimeout", clear_timeout)?;
+    Ok(())
+}
+
+#[module_init]
+fn init() {
+    pre_init();
+}
