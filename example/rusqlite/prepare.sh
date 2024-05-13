7a56c527606dc134f1800ad131611764ec967085
southorange0929 <southorange0929@foxmail.com>
2024-05-13 15:16:01 +0800

    feat: support cipher

---
+++ example/rusqlite/prepare.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export OPENSSL_NO_VENDOR=1
+export AARCH64_UNKNOWN_LINUX_OHOS_OPENSSL_DIR="${PWD}/../../ohos-openssl/prelude/arm64-v8a/"
+export ARMV7_UNKNOWN_LINUX_OHOS_OPENSSL_DIR="${PWD}/../../ohos-openssl/prelude/armeabi-v7a/"
+export X86_64_UNKNOWN_LINUX_OHOS_OPENSSL_DIR="${PWD}/../../ohos-openssl/prelude/x86_64/"
